65c6e36f93be5c52413e1c1ec2cb2b63ee4ec215
Войченко Виталий <mrabbat69@gmail.com>
2017-09-09 02:27:39 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,3 @@
-#<img src="/upload/shield/superbrands-50-l.jpg">
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,13 +5,21 @@ def getHtml (url):
     r = requests.get(url)
     return r.text
 
-def getSoup_appearedOrNot (html):
+def getSoup_appearedOrNot (html, pageNumber):
     soup = BeautifulSoup (html, 'html.parser')
     soupForCheking = soup.find_all ('img')
-    return soupForCheking
+    howManyTimes = 0
+    for i in soupForCheking:
+        print (i, '\n')
+        if ('upload/shield/superbrands-50-l.jpg' in i):
+            howManyTimes += 1
+
+    print (str(howManyTimes), 'СОВПАДЕНИЙ НА ', str(pageNumber), 'СТРАНИЦЕ!\n')            
 
 urls = ['https://www.eldorado.ru/special/550985894/?filter_category[1624320]=On',
         'https://www.eldorado.ru/special/550985894/page/2/?filter_category[1624320]=On',
         'https://www.eldorado.ru/special/550985894/page/3/?filter_category[1624320]=On']
 
-print (getSoup_appearedOrNot(getHtml(urls[0])))
+getSoup_appearedOrNot(getHtml(urls[0]), 1)
+getSoup_appearedOrNot(getHtml(urls[1]), 2)
+getSoup_appearedOrNot(getHtml(urls[2]), 3)
